f7052669511d8986b8391df2732b8d5b84051576
Blake Montgomery <blakemontgomery312@gmail.com>
2024-07-19 17:11:58 -0500

    First Draft Responsiveness

--- app/components/infopages.tsx
+++ app/components/infopages.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function InfoPages() {
   return (
     <>
-      <section className="PAGETWO bg-neutral-200 flex flex-row-reverse justify-center items-center text-justify px-36 pt-8 pb-12">
+      <section className="PAGETWO bg-neutral-200 flex flex-row-reverse justify-center items-center text-justify px-12 lg:px-36 pt-8 pb-12">
         <div className="flex justify-end items-end gap-12">
           <div className="IMAGE w-1/3 rounded-lg overflow-hidden mix-blend-multiply flex justify-start items-start">
             <Image
@@ -13,24 +13,22 @@ export default function InfoPages() {
               alt="Dickens reading..."
             />
           </div>
-          <div className="w-2/3 flex flex-col justify-end items-start pr-12">
-            <h2 className="text-neutral-900 text-2xl font-bold mb-2">The History</h2>
-            <p className="text-neutral-900 text-lg">In 1853, ten years after having dashed off his surprisingly successful holiday story, <span className="italic">A Christmas Carol</span>, Charles Dickens gave his first public reading of the work. The effort was so well received, as well as providing much needed cash, that Dickens continued to perform these celebrated readings for the rest of his life. In fact, it is believed that his death in 1870 was a direct result of these performances.</p>
+          <div className="w-2/3 flex flex-col justify-end items-start pr-0 lg:pr-12">
+            <h2 className="text-neutral-900 text-xl lg:text-2xl font-bold mb-2">The History</h2>
+            <p className="text-neutral-900 text-md lg:text-lg">In 1853, ten years after having dashed off his surprisingly successful holiday story, <span className="italic">A Christmas Carol</span>, Charles Dickens gave his first public reading of the work. The effort was so well received, as well as providing much needed cash, that Dickens continued to perform these celebrated readings for the rest of his life. In fact, it is believed that his death in 1870 was a direct result of these performances.</p>
           </div>
 
         </div>
       </section>
-      <section className="PAGETHREE bg-neutral-800 flex flex-row gap-4 py-16 px-24">
-        <div className="w-1/2 text-neutral-400 text-lg text-justify">
-          <h2 className="text-neutral-300 text-2xl font-bold mb-2">The Premise</h2>
-          <div className="flex flex-col gap-6">
-
-            <p>In creating this show, Blake Montgomery conflates the enduring popularity of the ubiquitous Christmas Carol with Dickens&apos; readings and imagines the reports of the author&apos;s death were exaggerated. And so, year after year, each and every December, not only do theaters around the globe produce adaptations of his famous book but Dickens himself is brought back to the stage to continue his tradition of solo readings ad infinitum. Whether this miracle is the result of the magic of Christmas, the power of the author&apos;s popularity, or a freak accident resulting from the interaction between an over-eager booking agent and some hitherto unappreciated arcana of contract law, it is impossible to state for certain.</p>
-            <p>After 171 years of consecutive readings, Dickens has reached a breaking point. Unbeknownst to the theater management, he will not be performing his show this evening. Tonight will be something entirely different. Something new. Something he is calling, in his head, &ldquo;Charles Dicken&apos;s First Annual Holiday Party.&rdquo; Perhaps Dickens wil get his wish. Or, perhaps, the Spirits of Christmas might intervene and force him to perform his worn-out tale once again. Perhaps, in the course of this performance, following some highly irregular and ridiculous occurrence, he might be forced to re-examine his creation; rediscover its beauty, and re-ignite his passion; al the while, along the way, forging the kind of intimate, joyful connection with the audience that he had hoped would come from hosting a party.</p>
-            <p>Perhaps Dickens wil get his wish. Or, perhaps, the Spirits of Christmas might intervene and force him to perform his worn-out tale once again. Perhaps, in the course of this performance, following some highly irregular and ridiculous occurrence, he might be forced to re-examine his creation; rediscover its beauty, and re-ignite his passion; al the while, along the way, forging the kind of intimate, joyful connection with the audience that he had hoped would come from hosting a party.</p>
+      <section className="PAGETHREE bg-neutral-800 flex flex-row gap-4 lg:gap-8 p-12 lg:p-24">
+        <div className="w-2/3 lg:w-1/2 text-neutral-400 text-justify">
+          <h2 className="text-neutral-300 text-xl lg:text-2xl font-bold mb-2">The Premise</h2>
+          <div className="flex flex-col gap-4 lg:gap-6">
+            <p className="text-md lg:text-lg">In creating this show, Blake Montgomery conflates the enduring popularity of the ubiquitous Christmas Carol with Dickens&apos; readings and imagines the reports of the author&apos;s death were exaggerated. And so, year after year, each and every December, not only do theaters around the globe produce adaptations of his famous book but Dickens himself is brought back to the stage to continue his tradition of solo readings ad infinitum. Whether this miracle is the result of the magic of Christmas, the power of the author&apos;s popularity, or a freak accident resulting from the interaction between an over-eager booking agent and some hitherto unappreciated arcana of contract law, it is impossible to state for certain.</p>
+            <p className="text-md lg:text-lg">After 171 years of consecutive readings, Dickens has reached a breaking point. Unbeknownst to the theater management, he will not be performing his show this evening. Tonight will be something entirely different. Something new. Something he is calling, in his head, &ldquo;Charles Dicken&apos;s First Annual Holiday Party.&rdquo; Perhaps Dickens wil get his wish. Or, perhaps, the Spirits of Christmas might intervene and force him to perform his worn-out tale once again. Perhaps, in the course of this performance, following some highly irregular and ridiculous occurrence, he might be forced to re-examine his creation; rediscover its beauty, and re-ignite his passion; al the while, along the way, forging the kind of intimate, joyful connection with the audience that he had hoped would come from hosting a party.</p>
           </div>
         </div>
-        <div className="w-1/2 flex flex-col gap-8 pl-12">
+        <div className="w-1/3 lg:w-1/2 flex flex-col gap-2 lg:gap-4 lg:pr-12">
 
           <div className="IMAGE rounded-lg overflow-hidden border border-neutral-400">
             <Image
@@ -53,19 +51,15 @@ export default function InfoPages() {
         </div>
       </section>
 
-      <section className="PAGETHREE bg-neutral-200 flex flex-row-reverse py-24 px-24">
-        <div className="w-1/2 text-neutral-500 text-lg text-justify">
-          <h2 className="text-neutral-700 text-3xl font-bold mb-8">The Artist</h2>
+      <section className="PAGETHREE bg-neutral-200 flex flex-row-reverse gap-4 lg:gap-8 p-12 lg:p-24">
+        <div className="w-2/3 lg:w-1/2 text-neutral-500 text-justify">
+          <h2 className="text-neutral-700 text-xl lg:text-3xl font-bold mb-2 lg:mb-4">The Artist</h2>
           <div className="flex flex-col gap-6">
-            <p>Blake Montgomery&apos;s career in the theater spans both the psychological- and text-based tradition as well as the physical, devised world of movement, mime, and clown theater. Having spent his early career as a director of ensemble clown theater, developed working relationships with non-naturalistic Chicago theater most notably Redmoon, he founded the Building Stage where he served as Artistic Director until its closing in 2013. The company specialized in ensemble-devised, director-driven works, most successfully  working with literary adaptations like their Jeff Award-winning Moby-Dick and poetic, clown-inspired Dawn, Quixote. Blake&apos;s work reflects his training at the Ecole Jacques Lecoq, the Dell&apos;Arte School of Physical Theater, and Middlebury College; he is a member of Actors&apos; Equity.</p>
-            <p className="">
-              “<span className="text-bold">
-                Charles Dickens Begrudgingly…
-              </span>
-              is a both the most terrifying and satisfying project Blake has created. It exemplifies his belief in a play being the interplay between structure and the performers sense of play. It combines his interest in theatrical clowning, love of unique literary narrative voices, traditional acting, and hospitality to create a uniquely personal, warm evening that both skewers and celebrates the famed author, our attachment to traditions, and the spirit of Christmas itself.</p>
+            <p className="text-md lg:text-lg">Blake Montgomery&apos;s career in the theater spans both the psychological- and text-based tradition as well as the physical, devised world of movement, mime, and clown theater. Having spent his early career as a director of ensemble clown theater, developed working relationships with non-naturalistic Chicago theater most notably Redmoon, he founded the Building Stage where he served as Artistic Director until its closing in 2013. The company specialized in ensemble-devised, director-driven works, most successfully  working with literary adaptations like their Jeff Award-winning Moby-Dick and poetic, clown-inspired Dawn, Quixote. Blake&apos;s work reflects his training at the Ecole Jacques Lecoq, the Dell&apos;Arte School of Physical Theater, and Middlebury College; he is a member of Actors&apos; Equity.</p>
+            <p className="text-md lg:text-lg"><span className="font-bold">Charles Dickens Begrudgingly…</span> is a both the most terrifying and satisfying project Blake has created. It exemplifies his belief in a play being the interplay between structure and the performers sense of play. It combines his interest in theatrical clowning, love of unique literary narrative voices, traditional acting, and hospitality to create a uniquely personal, warm evening that both skewers and celebrates the famed author, our attachment to traditions, and the spirit of Christmas itself.</p>
           </div>
         </div>
-        <div className="w-1/2 flex flex-col gap-8 pr-8">
+        <div className="w-1/3 lg:w-1/2 flex flex-col gap-8">
 
           <div className="IMAGE rounded-lg overflow-hidden border border-neutral-400">
             <Image
@@ -81,22 +75,15 @@ export default function InfoPages() {
 
       <section className="bg-black flex justify-center items-center bg-under-dickens bg-cover h-screen">
 
-        {/* <div className="IMAGE rounded-lg overflow-hidden border border-neutral-400">
-          <Image
-            src="/dickens_color4.jpg"
-            width="800"
-            height="800"
-            alt="Dickens reading..."
-          />
-        </div> */}
+        {/* IMAGE AS BACKGROUND */}
 
       </section>
 
 
-      <section className="p-12 flex flex-col justify-center items-center bg-neutral-200 text-neutral-900">
-        <h2 className="text-2xl mb-8 uppercase">Audience Reactions<span className="text-sm relative top-[-9px] left-[2px]">*</span></h2>
-
-        <ul className="text-center flex flex-col gap-4 w-3/4 text-lg">
+      <section className="p-12 lg:p-24 flex flex-col justify-start items-center bg-neutral-200 text-neutral-900">
+        <h2 className="text-2xl lg:text-3xl">Audience Reactions</h2>
+        <p className="italic text-xs lg:text-sm text-center">Specifically, comments from Goldstar members who attended the <br />2012 production at The Building Stage.</p>
+        <ul className="text-center flex flex-col gap-4 lg:gap-6 w-3/4 text-md lg:text-lg mt-8 lg:mt-12 max-w-[800px]">
 
           <li>
             The sleeper Christmas event of the season. You don&lsquo;t have to be into Dickens to appreciate it. It is so well done. I want to see it again.
@@ -133,14 +120,13 @@ export default function InfoPages() {
             Exactly what I needed to connect to the spirit of the season! Fabulous talent, wonderful humor, inspiring and insightful. <br />Thank you - Thank you - Thank you!
           </li>
         </ul>
-        <p className="italic text-sm text-center mt-24">*Specifically, comments from Goldstar members who attended the <br />2012 production at The Building Stage.</p>
       </section>
 
 
-      <section className="p-12 flex flex-col justify-center items-center bg-neutral-200 text-neutral-900">
-        <h2 className="text-2xl mb-16 uppercase">Press Reviews<span className="text-sm relative top-[-9px] left-[2px]">*</span></h2>
+      <section className="p-12 lg:p-24 flex flex-col justify-start items-center bg-neutral-400 text-neutral-900">
+        <h2 className="text-2xl lg:text-3xl">Press Reviews</h2>
 
-        <ul className="text-center flex flex-col gap-4 w-3/4">
+        <ul className="text-center flex flex-col gap-4 lg:gap-6 w-3/4 text-md lg:text-lg mt-8 lg:mt-12 max-w-[800px]">
 
           <li>
             The sleeper Christmas event of the season. You don&lsquo;t have to be into Dickens to appreciate it. It is so well done. I want to see it again.
@@ -164,9 +150,13 @@ export default function InfoPages() {
           </li>
         </ul>
       </section>
-      <section className="bg-black flex justify-center items-center bg-footer-dickens bg-cover h-screen">
+      <section className="bg-black bg-footer-dickens bg-center bg-cover h-screen flex justify-center items-start p-24">
 
-
+        {/* IMAGE */}
+        <a href="mailto:blakemontgomery312@gmail.com?subject=Charles Dickens Begrudgingly...">
+          <button
+            className="bg-amber-200 text-neutral-800 rounded-full px-4 py-1 lg:px-8 lg:py-2 my-2 text-2xl lg:text-3xl">Get In Touch</button>
+        </a>
       </section>
 
     </>
--- app/page.tsx
+++ app/page.tsx
@@ -5,30 +5,30 @@ export default function Home() {
   return (
     <main className="bg-black w-screen h-min-screen">
       <div className="h-screen bg-black">
+        <div className="LANDING h-[90%] bg-cover-dickens bg-cover bg-no-repeat bg-right-top flex flex-col justify-start items-start">
 
-        <div className="LANDING h-[90%] bg-neutral-600 bg-cover-dickens bg-cover bg- bg-no-repeat bg-right-top flex flex-col justify-start items-start">
-
-          <div className="w-1/2 grow p-16 pt-12">
+          <div className="h-full p-8 pt-4 lg:p-16 lg:pt-12">
             <div className="text-neutral-400">
 
-              <h2 className="text-4xl mb-2">Charles Dickens</h2>
-              <h1 className="text-8xl text-neutral-100 mb-4 drop-shadow-lg">Begrudgingly</h1>
-              <h2 className="text-4xl">Performs </h2>
-              <h2 className="text-4xl">&lsquo;A Christmas Carol.&rsquo;</h2>
-              <h2 className="text-4xl mb-8">Again.</h2>
+              <h2 className="text-2xl lg:text-4xl lg:mb-2">Charles Dickens</h2>
+              <h1 className="text-6xl lg:text-8xl text-neutral-100 mb-2 lg:mb-4 drop-shadow-lg">Begrudgingly</h1>
+              <h2 className="text-2xl lg:text-4xl">Performs </h2>
+              <h2 className="text-2xl lg:text-4xl">&lsquo;A Christmas Carol.&rsquo;</h2>
+              <h2 className="text-2xl mb-4 lg:text-4xl lg:mb-8">Again.</h2>
             </div>
 
             <div className="text-neutral-100">
-              <h4 className="text-sm">created and performed by</h4>
-              <h3 className="text-xl">Blake Montgomery</h3>
+              <h4 className="text-sm lg:text-md">created and performed by</h4>
+              <h3 className="text-xl  lg:text-2xl">Blake Montgomery</h3>
             </div>
           </div>
 
-          <div className="text-md text-neutral-400 bg-neutral-600/25 px-16 py-8 flex flex-col items-center mx-16 mb-8 rounded-lg backdrop-blur border border-neutral-700">
+          <div className="text-sm lg:text-md text-neutral-400 bg-neutral-600/25 px-8 py-4 lg:px-16 lg:py-8 flex flex-col items-center mx-8 mb-4 lg:mx-16 lg:mb-8 rounded-lg backdrop-blur border border-neutral-700">
             <p className="mb-2 text-justify ">Originally produced by the Building Stage in December of 2011 and 2012, CHARLES DICKENS BEGRUDGINGLY... charmed audiences with its unique, ridiculous, and ultimately warm-hearted cheer. Now, Blake Montgomery is bringing his Jeff Award-winning show back for the 2024 holiday season.</p>
-            <button className="bg-amber-200 text-neutral-800 rounded-full px-8 py-2 my-2">Get In Touch</button>
-
-
+            <a href="mailto:blakemontgomery312@gmail.com?subject=Charles Dickens Begrudgingly...">
+              <button
+                className="bg-amber-200 text-neutral-800 rounded-full px-4 py-1 lg:px-8 lg:py-2 my-2">Get In Touch</button>
+            </a>
           </div>
         </div>
 
