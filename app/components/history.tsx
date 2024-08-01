654111a4b971e6ac069f62a3ca95a5fccb8ff6a4
Blake Montgomery <blakemontgomery312@gmail.com>
2024-08-01 16:22:10 -0500

    Text Changes

--- app/components/history.tsx
+++ app/components/history.tsx
@@ -16,8 +16,7 @@ export default function History() {
           </div>
           < div className="w-full landscape:w-2/3 text-neutral-700" >
             <h2 className="text-lg landscape:text-xl lg:text-2xl font-bold mb-1 landscape:mb-2 lg:mb-3" > The History </h2>
-            <p className="text-sm landscape:text-lg lg:text-xl mb-2 sm:mb-3 landscape:mb-4"> In 1853, ten years after having dashed off his surprisingly successful holiday story, <span className="italic" > A Christmas Carol</span>, Charles Dickens gave his first public reading of the work. </p>
-            <p className="text-sm landscape:text-lg lg:text-xl">The effort was so well received, as well as providing much needed cash, that Dickens continued to perform these celebrated readings for the rest of his life. In fact, it is believed that his death in 1870 was a direct result of these performances.</p >
+            <p className="text-sm landscape:text-lg lg:text-xl mb-2 sm:mb-3 landscape:mb-4"> In 1853, ten years after having dashed off his surprisingly successful holiday story, <span className="italic" > A Christmas Carol</span>, Charles Dickens gave his first public reading of the work. The effort was so well received, as well as providing much needed cash, that Dickens continued to perform these celebrated readings for the rest of his life.</p >
           </div>
 
 
