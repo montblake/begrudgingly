277ec4fc4afa9d7fd805ede959b112e8704faf11
Blake Montgomery <blakemontgomery312@gmail.com>
2024-08-02 16:01:16 -0500

    Adjust call to action at breakpoints

--- app/components/call_to_action.tsx
+++ app/components/call_to_action.tsx
@@ -3,10 +3,10 @@ import { Mail } from 'lucide-react';
 
 export default function CallToAction() {
   return (
-    <div className="w-full flex flex-col justify-center items-center bg-neutral-700 rounded-lg p-8 sm:p-16 md:p-24 lg:p-32 xl:36 text-neutral-200 mt-4">
+    <div className="w-full flex flex-col justify-center items-center bg-neutral-700 rounded-lg p-8 sm:p-16 md:p-24 lg:p-32 xl:36 text-neutral-200 mt-4 landscape:p-8 landscape:mt-0 landscape:w-1/3">
 
-      <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl tracking-tight uppercase text-center">Seeking a venue for this year&rsquo;s performances</p>
-      <ul className="text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl list-disc list-outside p-4 flex flex-col gap-2 text-neutral-200">
+      <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl tracking-tight uppercase text-center landscape:text-xl">Seeking a venue for this year&rsquo;s performances</p>
+      <ul className="text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl landscape:text-xs list-disc list-outside p-4 flex flex-col gap-2 text-neutral-200">
         <li>
           Depending on availability and house size, performances could be held the weekend, week, or few weeks leading up to December 25.
         </li>
--- app/components/news.tsx
+++ app/components/news.tsx
@@ -7,14 +7,17 @@ export default function News() {
 
       <div className="w-full flex justify-center bg-neutral-200 rounded-md border border-neutral-600">
 
-        < div className="landscape:w-2/3 text-neutral-700 flex flex-col p-4 pb-8 sm:p-16 sm:pt-12 md:p-24 md:pt-16 lg:p-48 lg:pt-36   rounded-md landscape:my-8 landscape:pt-16 landscape:pb-24 " >
-          <h2 className="text-lg landscape:text-xl sm:text-xl md:text-2xl lg:text-3xl font-bold mb-0 landscape:mb-2 sm:mb-1 md:mb-2 lg:mb-2" > The News</h2>
+        < div className="landscape:w-[80%] text-neutral-700 flex flex-col p-4 pb-8 sm:p-16 sm:pt-12 md:p-24 md:pt-16 rounded-md landscape:px-0 landscape:my-8 landscape:pt-16 landscape:pb-24 landscape:flex-row landscape:gap-8" >
+          <div className="landscape:w-2/3">
 
-          <p className="text-sm landscape:text-lg sm:text-lg md:text-xl lg:text-2xl mb-2 sm:mb-3 landscape:mb-4">
-            <span className="uppercase text-sm sm:text-base md:text-lg lg:text-lg">Charles Dickens Begrudgingly</span> charmed audiences when first produced in Chicago at the Building Stage in December of 2011 and 2012.
-            This unique holiday show, which imagines a still-living Dickens forced to perform his famous <span className="uppercase text-sm sm:text-base md:text-lg lg:text-lg">Christmas Carol</span> for the 171st year in a row, lovingly celebrates and skewers the famous author and our Christmas traditions.</p>
-          <p className="text-sm landscape:text-lg sm:text-lg md:text-xl lg:text-2xl">Now, ten years after the closing the theater, former Building Stage Artistic Director Blake Montgomery brings his Jeff Award-winning show back to the stage in time for the 2024 holiday season.</p>
+            <h2 className="text-lg landscape:text-xl sm:text-xl md:text-2xl lg:text-3xl font-bold mb-0 landscape:mb-2 sm:mb-1 md:mb-2 lg:mb-2" > The News</h2>
 
+            <p className="text-sm landscape:text-lg sm:text-lg md:text-xl lg:text-2xl mb-2 sm:mb-3 landscape:mb-4">
+              <span className="uppercase text-sm sm:text-base md:text-lg lg:text-lg">Charles Dickens Begrudgingly</span> charmed audiences when first produced in Chicago at the Building Stage in December of 2011 and 2012.
+              This unique holiday show, which imagines a still-living Dickens forced to perform his famous <span className="uppercase text-sm sm:text-base md:text-lg lg:text-lg">Christmas Carol</span> for the 171st year in a row, lovingly celebrates and skewers the famous author and our Christmas traditions.</p>
+            <p className="text-sm landscape:text-lg sm:text-lg md:text-xl lg:text-2xl">Now, ten years after the closing the theater, former Building Stage Artistic Director Blake Montgomery brings his Jeff Award-winning show back to the stage in time for the 2024 holiday season.</p>
+
+          </div>
           <CallToAction />
 
         </div>
