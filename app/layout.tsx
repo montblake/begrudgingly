f404e3b9dc908e24bea485c6428f603a4a06728b
Blake Montgomery <blakemontgomery312@gmail.com>
2024-08-03 10:44:17 -0500

    Add Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Libre_Baskerville } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 const baskerville = Libre_Baskerville({ subsets: ["latin"], weight: ["400", "700"] })
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={baskerville.className}>{children}</body>
+      <body className={baskerville.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
