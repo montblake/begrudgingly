346c8142204035e6488d281ccce4107320aa5ffa
Blake Montgomery <blakemontgomery312@gmail.com>
2024-10-28 00:33:37 -0500

    Performance Page Vertical Spacing

--- app/performances/page.tsx
+++ app/performances/page.tsx
@@ -6,10 +6,10 @@ import Policies from "@/app/components/policies";
 
 export default function Performances() {
   return (
-    <main className="w-full h-min-screen relative z-50 text-gray-400 flex flex-col justify-between items-center bg-black mt-10">
+    <main className="w-full min-h-screen relative z-50 text-gray-400 flex flex-col justify-between items-center bg-black mt-10">
       <Nav />
 
-      <div className="max-w-screen-xl h-full pt-12 px-4 sm:px-12 lg:px-16 pb-12 lg:pt-8">
+      <div className="max-w-screen-xl w-full flex-grow pt-12 px-4 sm:px-12 lg:px-16 pb-12 lg:pt-8">
         <div className="w-full flex flex-col lg:flex-row justify-center items-center lg:gap-16 lg:items-start mb-8">
           <Venue />
           <TicketLinks />
