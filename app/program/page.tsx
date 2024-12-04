db26c07cb442ed2264eafcd2870b985015153d8e
Blake Montgomery <blakemontgomery312@gmail.com>
2024-12-04 09:47:24 -0600

    Add Program Page

---
+++ app/program/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Link from "next/link";
+
+export default function Program() {
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-100 text-black">
+      <h1>Dickens Program 2024</h1>
+    </div>
+  );
+}
