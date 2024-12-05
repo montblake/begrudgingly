faff571453c16dd9d72eb06db0ca4b0959549d0a
Blake Montgomery <blakemontgomery312@gmail.com>
2024-12-05 11:45:36 -0600

    Bios In Process

--- app/program/page.tsx
+++ app/program/page.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Program() {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-100 text-black p-12">
-      <div className="flex flex-col items-center justify-center">
+    <div className="min-h-screen flex flex-col items-center justify-start bg-gray-100 text-black p-12">
+      <div className="flex flex-col items-center justify-center max-w-3xl w-full">
         <h1 className="font-bold text-center mb-12">
           <span className="text-4xl">Charles Dickens</span>
           <br />
@@ -50,9 +50,9 @@ export default function Program() {
           <h2 className="text-xl font-bold mb-2">Sion Silva</h2>
         </div>
         <div className="flex flex-col items-center justify-center mb-8">
-          <h5 className="text-base italic">House Management</h5>
+          <h5 className="text-base italic">house management</h5>
           <h2 className="text-xl font-bold mb-2">Andrew Snyder</h2>
-          <h5 className="text-base italic">Marketing and Media</h5>
+          <h5 className="text-base italic">marketing and media</h5>
           <h2 className="text-xl font-bold mb-2">SHOUT Marketing</h2>
         </div>
         <div className="flex flex-col items-center justify-center mb-8">
@@ -62,6 +62,108 @@ export default function Program() {
             <br />
             Lauren Pizzi, Mieka Van Der Ploeg
           </h2>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-start max-w-xl">
+        <h1 className="text-2xl font-bold mb-4 mt-12">Company Biographies</h1>
+        <div className="flex flex-col items-center justify-start gap-4">
+          <p className="text-base">
+            Blake Montgomery (Dickens) began his career in Chicago directing
+            ensemble clown theater and performing with a variety of
+            non-naturalistic Chicago theaters, most notably Redmoon with whom he
+            collaborated for more than a decade. In 2005, Montgomery founded The
+            Building Stage, a theater company and performance space, where he
+            served as Artistic Director until its closing in 2013. The company
+            was known for its ensemble-devised, director-driven creations,
+            particularly in literary adaptations like their Jeff Award-winning
+            Moby-Dick and the poetic, clown-inspired Dawn, Quixote. Montgomery's
+            artistic approach draws on his training at the École Jacques Lecoq,
+            the Dell'Arte School of Physical Theater, and Middlebury College. A
+            member of Actors' Equity, his performances in Chicago include work
+            with The House Theatre, The Hypocrites, Remy Bumppo Theatre Company,
+            Court Theatre, Chicago Shakespeare, Steppenwolf, and Writers
+            Theatre. Honoring the Past, Present, and Future: this performance is
+            dedicated to Anne, Lauren, and Bodie.
+          </p>
+          <p className="text-base">
+            Izumi Inaba (costume designer) is so delighted to work with Blake
+            again! Previously with him, she has designed Charles Dickens
+            Begrudgingly Performs ‘A Christmas Carol,’ Again and Moby Dick at
+            Building Stage back in 2011. Her recent works include Noises Off
+            (Steppenwolf Theatre Company), I Am Not Your Perfect Mexican
+            Daughter (Denver Center Performing Arts), Milo Imagines The World
+            (Chicago Children’s Theatre), Every Brilliant Thing (Writers
+            Theatre), The Comedians (Ravens Theatre). Upcoming projects include
+            Empty Ride (Old Globe), The Play That Goes Wrong (Indiana Repartory
+            Theatre), and Cats (Paramount Theatre). Izumi is a member of United
+            Scenic Artists Local USA 829 and represented by Gersh.
+          </p>
+          <p className="text-base">
+            Megan E. Pirtle (Wig Designer) is a Chicago based theatre artist
+            specialized in wig, costume, and makeup design. Her previous designs
+            for Haymarket include La liberazione di Ruggiero dall’isola
+            d’Alcina, Marc’ Antonio e Cleopatra, L’incoronazione di Poppea, and
+            L’amant Anonyme. Selected wig design credits include Trouble in
+            Mind, Campaigns Inc. (co-design), Relentless, Rutherford & Son
+            (TimeLine), Mesmerized (Chicago Children’s Theatre) The October
+            Storm, The Last Pair of Earlies, Crumbs from the Table of Joy
+            (Raven), Gypsy, Hello Dolly (co-design), You’re a Good Man Charlie
+            Brown, and The Princess & the Pea (Marriott). She has also worked as
+            the Wig & Hair Coordinator and Artisan for Villette, Her Honor Jane
+            Byrne (Lookingglass), Two Trains Running (Court), Ring of Fire
+            (Drury Lane), Dial M for Murder and Georgiana & Kitty (Northlight).
+            Most recently, Megan was the Makeup Coordinator for the Chicago
+            premiere of Emmet Otter’s Jug- Band Christmas (Timothy Allen
+            McDonald/ iTheatrics). Megan is also a licensed cosmetologist of
+            Illinois. And in 2023, she was the recipient of the Michael Merritt
+            Emerging Technical Collaborator Award. For more, visit
+            meganpirtledesign.com.
+          </p>
+          <p className="text-base">
+            Oskar Westbridge (Sound Design) is a director, sound designer, and
+            teaching artist. They recently directed/sound designed R.U.R
+            (Rossum’s Universal Robots) and Frankenstein as a guest artist at
+            Triton College. They are a graduate of The Theater School at DePaul
+            University, a faculty member of the National High School Institute
+            (“Cherubs”) program, and a alumni of Directors Lab North 2024. See
+            more at oskarcywestbridge.com.
+          </p>
+          <p className="text-base">
+            GEORGE KEATING (Ghost of Rehearsals Present) — George is honored to
+            hover about rehearsals and attempt to say helpful things. When it’s
+            not Christmas, he can be seen teaching at The Theatre School at
+            DePaul University. He appears on stages in Chicago for the past 30
+            years and occasionally on your television or social media platform.
+            George is the Theatre Arts Division Director of The National High
+            School Institute at Northwestern University, more commonly known as
+            the cherubs program. He plans to make theatre and music, and be
+            around art, until he is inevitably compared to the Dickensian
+            “doornail.”
+          </p>
+          <p className="text-base">
+            Jake Snell (Stage Management) is from Palos Heights, IL and
+            currently resides in Rogers Park as a freelance stage and production
+            manager. Jake is a recent graduate from Loyola University Chicago
+            where he studied theater and psychology. Recent credits include work
+            with Goodman Theatre, Northlight Theatre, Congo Square Theatre,
+            Black Ensemble Theatre, Definition Theatre, Griffin Theatre,
+            Porchlight Music Theatre, and Trap Door Theatre. Jake would like to
+            thank his family and friends for their endless love and support.
+          </p>
+          <p className="text-base">
+            Thompson MacKenzie Scribner (Board Operator) is brand new to
+            Chicago! She loves big rocks, small rocks, and animals with weird
+            organs. You can find her blog at thompsonscribner.com or on
+            substack.com/@thompsonmac.
+          </p>
+          <p className="text-base">
+            Andrew Snyder (House Management) is a Theatre Artist/Musician from
+            Fort Smith, AR. Previous Chicago credits include Stage Management
+            for "By the Way, Meet Vera Stark", "The Pavilion" and "Dying For It"
+            with The Artistic Home. Andrew holds a B.A. in Acting/Directing from
+            the University of Arkansas at Fort Smith and an M.F.A in Acting from
+            Northern Illinois University.
+          </p>
         </div>
       </div>
     </div>
