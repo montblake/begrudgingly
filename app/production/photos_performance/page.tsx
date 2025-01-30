5bece531b6da95b08a9412382ba8738cd6d0b9d5
Blake Montgomery <blakemontgomery312@gmail.com>
2025-01-30 01:46:17 -0600

    Add Carousel

--- app/production/photos_performance/page.tsx
+++ app/production/photos_performance/page.tsx
@@ -1,4 +1,15 @@
+"use client";
+
 import Image from "next/image";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselPrevious,
+  CarouselNext,
+} from "@/components/ui/carousel";
+import { type CarouselApi } from "@/components/ui/carousel";
+import { useEffect, useState } from "react";
 
 const photos = [
   {
@@ -126,9 +137,19 @@ const photos = [
 ];
 
 export default function Home() {
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+
+  useEffect(() => {
+    if (!api) return;
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap());
+    });
+  }, [api]);
   return (
-    <main className="w-full h-fit py-8 bg-red-950/50">
-      <div className="relative w-full">
+    <main className="w-full h-full py-0">
+      {/* <div className="relative w-full">
         <div className="fixed z-40 top-36 left-0 w-full flex flex-col justify-center items-center">
           <h1 className="text-xs text-neutral-500 italic border border-neutral-500 rounded-xl px-4 py-2 -mt-2  bg-black/50">
             First Preview, photos by{" "}
@@ -141,9 +162,9 @@ export default function Home() {
             </a>
           </h1>
         </div>
-      </div>
+      </div> */}
 
-      <div className="w-full h-full flex flex-col justify-start items-center gap-y-8 md:gap-y-16 px-4 pt-24 pb-24 overflow-y-scroll">
+      {/* <div className="w-full h-full flex flex-col justify-start items-center gap-y-8 md:gap-y-16 px-4 pt-24 pb-24 overflow-y-scroll">
         {photos.map((photo, index) => (
           <Image
             key={index}
@@ -158,6 +179,55 @@ export default function Home() {
             height={800}
           />
         ))}
+      </div> */}
+      <div className="w-full h-full flex flex-col items-center justify-center bg-neutral-950 max-w-[800px] mx-auto rounded-2xl pl-4">
+        <Carousel
+          className="w-full md:max-w-[600px] max-h-[600px] flex flex-col justify-center items-center"
+          setApi={setApi}
+        >
+          <CarouselContent className="w-full">
+            {photos.map((photo, index) => (
+              <CarouselItem key={index} className={`w-full`}>
+                <Image
+                  src={photo.src}
+                  alt={photo.alt}
+                  width={800}
+                  height={800}
+                  className={`rounded-2xl border-[.5px] border-neutral-700 mx-auto ${
+                    photo.vertical
+                      ? "h-full w-auto max-h-[60vh]"
+                      : "w-full h-auto max-w-[800px]"
+                  }`}
+                />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+        <div className="relative -left-2">
+          <div className="mt-4 pb-3 flex gap-2 justify-center items-center">
+            {photos.map((_, index) => (
+              <div
+                key={index}
+                className={`w-2 h-2 rounded-full transition-all duration-300 ease-in-out ${
+                  current === index ? "bg-neutral-200" : "bg-neutral-500"
+                }`}
+              />
+            ))}
+          </div>
+          <h1 className="text-xs text-center text-neutral-500 italic px-4 mb-0">
+            Photos of first preview by{" "}
+            <a
+              href="https://www.brosilow.com/"
+              target="_blank"
+              className="text-neutral-200 hover:underline"
+            >
+              Michael Brosilow
+            </a>
+          </h1>
+        </div>
       </div>
     </main>
   );
--- app/production/photos_publicity/page.tsx
+++ app/production/photos_publicity/page.tsx
@@ -1,4 +1,15 @@
+"use client";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { type CarouselApi } from "@/components/ui/carousel";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const photos = [
   {
@@ -45,10 +56,20 @@ const photos = [
 ];
 
 export default function PhotosPublicity() {
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+
+  useEffect(() => {
+    if (!api) return;
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap());
+    });
+  }, [api]);
   return (
-    <main className="w-full h-fit py-8 bg-red-950/50">
-      <div className="relative w-full">
-        <div className="fixed z-40 top-36 left-0 w-full flex flex-col justify-center items-center">
+    <main className="w-full h-full py-8">
+      {/* <div className="relative w-full"> */}
+      {/* <div className="fixed z-40 top-36 left-0 w-full flex flex-col justify-center items-center">
           <h1 className="text-xs text-neutral-500 italic border border-neutral-500 rounded-xl px-4 py-2 -mt-2 bg-black/50">
             Publicity photos by{" "}
             <a
@@ -59,10 +80,10 @@ export default function PhotosPublicity() {
               Joe Mazza / brave lux inc.
             </a>
           </h1>
-        </div>
-      </div>
+        </div> */}
+      {/* </div> */}
 
-      <div className="w-full h-full flex flex-col justify-start items-center gap-y-8 md:gap-y-16 px-4 pt-24 pb-24 overflow-y-scroll">
+      {/* <div className="w-full h-full flex flex-col justify-start items-center gap-y-8 md:gap-y-16 px-4 pt-24 pb-24 overflow-y-scroll">
         {photos.map((photo, index) => (
           <Image
             key={index}
@@ -73,6 +94,48 @@ export default function PhotosPublicity() {
             height={800}
           />
         ))}
+      </div> */}
+      <div className="w-full h-full flex flex-col items-center justify-center bg-neutral-950 max-w-[800px] mx-auto rounded-2xl pl-4">
+        <Carousel className="w-full h-full" setApi={setApi}>
+          <CarouselContent className="w-full">
+            {photos.map((photo, index) => (
+              <CarouselItem key={index} className="w-full">
+                <Image
+                  src={photo.src}
+                  alt={photo.alt}
+                  width={800}
+                  height={800}
+                  className="rounded-2xl border-[.5px] border-neutral-700"
+                />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+        <div className="relative -left-2">
+          <div className="mt-4 pb-3 flex gap-2 justify-center items-center">
+            {photos.map((_, index) => (
+              <div
+                key={index}
+                className={`w-2 h-2 rounded-full transition-all duration-300 ease-in-out ${
+                  current === index ? "bg-neutral-200" : "bg-neutral-400"
+                }`}
+              />
+            ))}
+          </div>
+          <h1 className="text-xs text-center text-neutral-500 italic px-4">
+            Publicity photos by{" "}
+            <a
+              href="https://www.bravelux.com/"
+              target="_blank"
+              className="text-neutral-200 hover:underline"
+            >
+              Joe Mazza / brave lux inc.
+            </a>
+          </h1>
+        </div>
       </div>
     </main>
   );
