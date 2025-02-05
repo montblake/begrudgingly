79bcb91a2a9726154e36ed65683b28306b261131
Blake Montgomery <blakemontgomery312@gmail.com>
2025-02-05 17:35:59 -0600

    Fix Contact CTA

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,4 @@
 import SpecialPage from "@/components/special_page";
-import { Mail } from "lucide-react";
 
 const ImageProps = {
   src: "/dickens_contact.jpg",
@@ -18,22 +17,19 @@ const HeadlineProps = {
   publisher: "",
 };
 
-const TextElement = (
-  <div className="w-[90%] mx-auto md:mx-0 mt-4 flex flex-col items-center bg-gradient-to-br from-neutral-200/75 to-neutral-500/100 py-4 px-8 rounded-xl gap-2">
-    <p className="text-xs text-neutral-950">
-      Inquiries for Blake Montgomery, or Charles Dickens, may be directed to:
-    </p>
-    <div className="my-2 w-full">
-      <a
-        href="mailto:dickens@dickensagain.com"
-        className="bg-neutral-900 border border-neutral-500 text-neutral-400 text-xs px-4 py-2 rounded-xl hover:text-neutral-300 transition-all duration-300 ease-in-out active:scale-95 w-full flex items-center justify-center gap-2"
-      >
-        <Mail className="w-4 h-4 text-neutral-400" />
-        <span className="hover:underline">dickens@dickensagain.com</span>
-      </a>
-    </div>
-  </div>
-);
+const TextElement = <></>;
+
+const CtaContent = {
+  intro:
+    "Inquiries for Blake Montgomery, or Charles Dickens, may be directed to:",
+  links: [
+    {
+      href: "mailto:dickens@dickensagain.com",
+      text: "dickens@dickensagain.com",
+      internal: false,
+    },
+  ],
+};
 
 export default function Contact() {
   return (
@@ -41,6 +37,7 @@ export default function Contact() {
       ImageProps={ImageProps}
       HeadlineProps={HeadlineProps}
       TextElement={TextElement}
+      CtaContent={CtaContent}
     />
   );
 }
