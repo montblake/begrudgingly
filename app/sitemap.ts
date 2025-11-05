5a826261cc9d7a99b57738ca9a9c04e0e95867bf
Blake Montgomery <blakemontgomery312@gmail.com>
2025-11-05 14:32:34 -0600

    Update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,49 +3,49 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://www.dickensagain.com",
+      url: "https://www.dickensagain.com/",
       lastModified: new Date(),
       changeFrequency: "always",
       priority: 1,
     },
     {
-      url: "https://www.dickensagain.com/history",
+      url: "https://www.dickensagain.com/history/",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.5,
     },
     {
-      url: "https://www.dickensagain.com/2024/reviews",
+      url: "https://www.dickensagain.com/2024/reviews/",
       lastModified: new Date(),
       changeFrequency: "never",
       priority: 0.1,
     },
     {
-      url: "https://www.dickensagain.com/2024/photos-publicity",
+      url: "https://www.dickensagain.com/2024/photos-publicity/",
       lastModified: new Date(),
       changeFrequency: "never",
       priority: 0.1,
     },
     {
-      url: "https://www.dickensagain.com/2024/photos-performance",
+      url: "https://www.dickensagain.com/2024/photos-performance/",
       lastModified: new Date(),
       changeFrequency: "never",
       priority: 0.1,
     },
     {
-      url: "https://www.dickensagain.com/2024/program",
+      url: "https://www.dickensagain.com/2024/program/",
       lastModified: new Date(),
       changeFrequency: "never",
       priority: 0.1,
     },
     {
-      url: "https://www.dickensagain.com/creator",
+      url: "https://www.dickensagain.com/creator/",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.1,
     },
     {
-      url: "https://www.dickensagain.com/contact",
+      url: "https://www.dickensagain.com/contact/",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.1,
