ba130ff1651f481be077cf51bd4c47a4a8fc3be9
Blake Montgomery <blakemontgomery312@gmail.com>
2025-11-05 14:20:13 -0600

    Logo always Link

--- components/header_logo.tsx
+++ components/header_logo.tsx
@@ -9,25 +9,21 @@ export default function HeaderLogo({ pathname }: { pathname: string }) {
     <ul className="flex space-x-6 items-center justify-center mr-8">
       {pathname === "/" ? (
         <li>
-          {/* <Tooltip title="Home" arrow>
-            <Link
-              href="/"
-              aria-label="Home"
-              scroll={false}
-              className="text-neutral-200 flex items-center focus:outline-none focus:border-b-2 focus:border-blue-400"
-            > */}
-          <Image
-            src="/cdb_logo.png"
-            alt="Home"
-            width={200}
-            height={32}
-            priority
-            className={`h-5 w-auto relative z-100 -top-[1px]  transition-all duration-300 opacity-90 mix-blend-screen ${
-              pathname === "/" ? "" : "hover:scale-95"
-            }`}
-          />
-          {/* </Link>
-          </Tooltip> */}
+          <Link
+            href="/"
+            aria-label="Home"
+            scroll={false}
+            className="text-neutral-200 flex items-center pointer-events-none"
+          >
+            <Image
+              src="/cdb_logo.png"
+              alt="Home"
+              width={200}
+              height={32}
+              priority
+              className={`h-5 w-auto relative z-100 -top-[1px] opacity-90 mix-blend-screen`}
+            />
+          </Link>
         </li>
       ) : (
         <li>
