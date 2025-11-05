d4a775f735543c1cd024f6054a7866bb121e1af8
Blake Montgomery <blakemontgomery312@gmail.com>
2025-11-05 00:33:58 -0600

    Add Robots.txt and Sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,54 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://dickensagain.com",
+      lastModified: new Date(),
+      changeFrequency: "always",
+      priority: 1,
+    },
+    {
+      url: "https://dickensagain.com/history",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+    {
+      url: "https://dickensagain.com/2024/reviews",
+      lastModified: new Date(),
+      changeFrequency: "never",
+      priority: 0.1,
+    },
+    {
+      url: "https://dickensagain.com/2024/photos-publicity",
+      lastModified: new Date(),
+      changeFrequency: "never",
+      priority: 0.1,
+    },
+    {
+      url: "https://dickensagain.com/2024/photos-performance",
+      lastModified: new Date(),
+      changeFrequency: "never",
+      priority: 0.1,
+    },
+    {
+      url: "https://dickensagain.com/2024/program",
+      lastModified: new Date(),
+      changeFrequency: "never",
+      priority: 0.1,
+    },
+    {
+      url: "https://dickensagain.com/creator",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.1,
+    },
+    {
+      url: "https://dickensagain.com/contact",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.1,
+    },
+  ];
+}
