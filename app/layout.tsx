c6539448ca006badf855c53a5da326c70a2368a6
Blake Montgomery <blakemontgomery312@gmail.com>
2025-11-06 14:14:41 -0600

    remove vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { Libre_Baskerville } from "next/font/google";
 import "./globals.css";
 import Header from "../components/header";
-import AnalyticsOnIdle from "./analyticsOnIdle";
+// import AnalyticsOnIdle from "./analyticsOnIdle";
 
 const baskerville = Libre_Baskerville({
   subsets: ["latin"],
@@ -24,7 +24,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
-        {process.env.NODE_ENV === "production" && <AnalyticsOnIdle />}
+        {/* {process.env.NODE_ENV === "production" && <AnalyticsOnIdle />} */}
       </body>
     </html>
   );
