6b7b54e52c90dc650ca8af982444039f4bb1b932
Blake Montgomery <blakemontgomery312@gmail.com>
2025-11-06 23:11:19 -0600

    remove unused tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,45 +6,12 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "shadow-dickens":
-          'url("/photos/bravelux/100-joe-mazza-chicago-brave-lux-inc-5206928.jpg")',
         "victorian-texture": "url('/victorian-texture_darker.webp')",
-        "red-dickens": 'url("/red-dickens-2.jpg")',
-      },
-      textShadow: {
-        "shadow-dickens": "0 0 4px rgba(0, 0, 0, 1)",
-      },
-      colors: {
-        "title-start": "#dcbf7d",
-        "title-middle": "cba95f",
-        "title-end": "#b48a34",
       },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
-      },
-      keyframes: {
-        "accordion-down": {
-          from: {
-            height: "0",
-          },
-          to: {
-            height: "var(--radix-accordion-content-height)",
-          },
-        },
-        "accordion-up": {
-          from: {
-            height: "var(--radix-accordion-content-height)",
-          },
-          to: {
-            height: "0",
-          },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
