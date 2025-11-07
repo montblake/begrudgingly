96cadfdc153138cbfa6442e4a31b352f3a362a98
Blake Montgomery <blakemontgomery312@gmail.com>
2025-11-07 10:32:34 -0600

    Replace Headshot with Optimized image

--- app/creator/page.tsx
+++ app/creator/page.tsx
@@ -2,7 +2,7 @@ import SpecialPage from "@/components/special_page";
 import { Metadata } from "next";
 
 const ImageProps = {
-  src: "/blake_headshot.webp",
+  src: "/blake_headshot_opt.webp",
   alt: "Blake Montgomery Headshot",
   width: 500,
   height: 655,
