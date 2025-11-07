d170363abd9d586a235cc6cf0b528c41ef1ee6c3
Blake Montgomery <blakemontgomery312@gmail.com>
2025-11-06 22:37:17 -0600

    Add images sizes in config and hero

--- next.config.mjs
+++ next.config.mjs
@@ -7,12 +7,12 @@ const nextConfig = {
     esmExternals: true, // prefer ESM builds of deps where available
   },
 
-  // images: {
-  //   formats: ["image/avif", "image/webp"],
-  //   deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-  //   imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-  //   minimumCacheTTL: 60,
-  // },
+  images: {
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 60,
+  },
 
   async redirects() {
     return [
