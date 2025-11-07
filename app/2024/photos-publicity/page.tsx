c1e27321daabe940295f6b19290b2c6a1082f903
Blake Montgomery <blakemontgomery312@gmail.com>
2025-11-07 11:00:38 -0600

    Joe Mazza Photos Optimized

--- app/2024/photos-publicity/page.tsx
+++ app/2024/photos-publicity/page.tsx
@@ -10,50 +10,50 @@ export const metadata: Metadata = {
 
 const photos = [
   {
-    src: "/photos/bravelux/1_joe-mazza-chicago-brave-lux-inc-5108630.jpg",
+    src: "/photos/bravelux/1_joe-mazza-chicago-brave-lux-inc-5108630.webp",
     alt: "Charles Dickens",
     credit: "Joe Mazza / brave lux inc.",
     vertical: false,
   },
   {
-    src: "/photos/bravelux/4-joe-mazza-chicago-brave-lux-inc-5206468.jpg",
+    src: "/photos/bravelux/4-joe-mazza-chicago-brave-lux-inc-5206468.webp",
     alt: "Charles Dickens",
     credit: "Joe Mazza / brave lux inc.",
     vertical: false,
   },
   {
-    src: "/photos/bravelux/joe-mazza-chicago-brave-lux-inc-5108673.jpg",
+    src: "/photos/bravelux/joe-mazza-chicago-brave-lux-inc-5108673.webp",
     alt: "Charles Dickens",
     credit: "Joe Mazza / brave lux inc.",
     vertical: false,
   },
 
   {
-    src: "/photos/bravelux/joe-mazza-chicago-brave-lux-inc-5206766.jpg",
+    src: "/photos/bravelux/joe-mazza-chicago-brave-lux-inc-5206766.webp",
     alt: "Charles Dickens",
     credit: "Joe Mazza / brave lux inc.",
     vertical: false,
   },
   {
-    src: "/photos/bravelux/joe-mazza-chicago-brave-lux-inc-5206555.jpg",
+    src: "/photos/bravelux/joe-mazza-chicago-brave-lux-inc-5206555.webp",
     alt: "Charles Dickens",
     credit: "Joe Mazza / brave lux inc.",
     vertical: false,
   },
   {
-    src: "/photos/bravelux/joe-mazza-chicago-brave-lux-inc-5206804-2.jpg",
+    src: "/photos/bravelux/joe-mazza-chicago-brave-lux-inc-5206804-2.webp",
     alt: "Charles Dickens",
     credit: "Joe Mazza / brave lux inc.",
     vertical: false,
   },
   {
-    src: "/photos/bravelux/202_joe-mazza-chicago-brave-lux-inc-5206819.jpg",
+    src: "/photos/bravelux/202_joe-mazza-chicago-brave-lux-inc-5206819.webp",
     alt: "Charles Dickens",
     credit: "Joe Mazza / brave lux inc.",
     vertical: false,
   },
   {
-    src: "/photos/bravelux/100-joe-mazza-chicago-brave-lux-inc-5206928.jpg",
+    src: "/photos/bravelux/100-joe-mazza-chicago-brave-lux-inc-5206928.webp",
     alt: "Charles Dickens",
     credit: "Joe Mazza / brave lux inc.",
     vertical: false,
