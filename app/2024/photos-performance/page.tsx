08e798f4c7c0f3d4544f2ef8fd04a1455fbf47e1
Blake Montgomery <blakemontgomery312@gmail.com>
2025-11-10 01:17:59 -0600

    Refactor Photos-Performance page

--- app/2024/photos-performance/page.tsx
+++ app/2024/photos-performance/page.tsx
@@ -144,17 +144,42 @@ const CtaProps = {
   ],
 };
 
+const textElement = (
+  <div className="w-full flex flex-col items-center justify-center bg-neutral-800 rounded-2xl p-4">
+    <p className="text-neutral-300 text-xs md:text-sm leading-relaxed">
+      These performance photos capture{" "}
+      <span className="font-semibold">
+        Charles Dickens Begrudgingly Performs A Christmas Carol Again
+      </span>
+      , created and performed by Blake Montgomery for the 2024 production at The
+      Den Theatre in Chicago. Photographed by Michael Brosilow during the live
+      first preview performance, these images showcase Montgomery&apos;s
+      portrayal of Charles Dickens in this one-man theatrical adaptation of the
+      classic Christmas tale. Brosilow&apos;s unmatched skill at capturing the
+      nuance and detail of a show in real-time is evident throughout these
+      photographs, which capture the production&apos;s perfect balance of
+      dramatic storytelling and playful humor, showcasing Montgomery&apos;s
+      intimate, off-the-cuff performance style that makes the familiar story
+      feel fresh and engaging. The CLOWNSHOW production ran from December 5-22,
+      2024, and received critical acclaim from Chicago theater critics.
+    </p>
+  </div>
+);
+
 export default function PhotosPerformance() {
   return (
-    <>
-      <PhotoGallery
-        key="/2024/photos-performance"
-        HeaderInfo={HeaderInfo}
-        photos={photos}
-        intro={CtaProps.intro}
-        links={CtaProps.links}
-      />
-      <div className="w-full max-w-3xl mx-auto px-4 sm:px-8 md:px-2 py-8 -mt-8">
+    <div className="w-full h-[calc(100vh-3rem)] p-4 sm:p-8 overflow-y-auto">
+      <div className="w-full h-full">
+        <PhotoGallery
+          key="/2024/photos-performance"
+          HeaderInfo={HeaderInfo}
+          photos={photos}
+          // intro={CtaProps.intro}
+          links={CtaProps.links}
+          textElement={textElement}
+        />
+      </div>
+      {/* <div className="w-full max-w-3xl mx-auto px-4 sm:px-8 md:px-2 py-8 -mt-8">
         <div className="bg-neutral-200/50 backdrop-blur-sm border border-neutral-700/30 rounded-xl p-6 md:p-12">
           <p className="text-neutral-300 text-base md:text-base leading-relaxed">
             These performance photos capture{" "}
@@ -176,7 +201,7 @@ export default function PhotosPerformance() {
           </p>
         </div>
         <div className="w-full h-24 md:h-36 bg-gradient-to-b from-neutral-950/0 to-neutral-950/100 md:bg-none" />
-      </div>
-    </>
+      </div> */}
+    </div>
   );
 }
