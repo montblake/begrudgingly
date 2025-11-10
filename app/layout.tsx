3c7aba2b58762ba02e161eade76a0f867ce45b1f
Blake Montgomery <blakemontgomery312@gmail.com>
2025-11-09 23:35:55 -0600

    lazy load analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
 import { Libre_Baskerville } from "next/font/google";
 import "./globals.css";
 import Header from "../components/header";
-import { Analytics } from "@vercel/analytics/react";
+// import { Analytics } from "@vercel/analytics/react";
+import dynamic from "next/dynamic";
+const Analytics = dynamic(
+  () => import("@vercel/analytics/react").then((m) => m.Analytics) // Lazy load analytics to reduce TBT
+);
 
 const baskerville = Libre_Baskerville({
   subsets: ["latin"],
