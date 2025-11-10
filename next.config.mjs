41c2f432d486af636189b729f735024e0f3640a6
Blake Montgomery <blakemontgomery312@gmail.com>
2025-11-09 20:52:49 -0600

    remove config image sizes

--- next.config.mjs
+++ next.config.mjs
@@ -26,9 +26,9 @@ const nextConfig = {
   },
 
   images: {
-    formats: ["image/avif", "image/webp"],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    formats: ["image/avif"],
+    // deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    // imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
     qualities: [50, 60, 75],
   },
